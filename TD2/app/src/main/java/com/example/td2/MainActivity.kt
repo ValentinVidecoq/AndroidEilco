199a06e50f251f07c79110792175d9d0122670cc
Valentin Videcoq <videcoq.valentin@gmail.com>
2021-11-17 11:55:52 +0100

    Add : TD2 Ex2

--- TD2/app/src/main/java/com/example/td2/MainActivity.kt
+++ TD2/app/src/main/java/com/example/td2/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.td2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.repas)
+
+        // Change this val to switch from 'repas' to 'meteo'
+        val contentView = R.layout.meteo
+
+        setContentView(contentView)
+
+        if (contentView == R.layout.meteo){
+            // Took this part from https://developer.android.com/guide/topics/ui/controls/spinner
+            val spinner: Spinner = findViewById(R.id.weather_spinner)
+            // Create an ArrayAdapter using the string array and a default spinner layout
+            ArrayAdapter.createFromResource(
+                this,
+                R.array.weather,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                spinner.adapter = adapter
+            }
+        }
+
     }
 }
